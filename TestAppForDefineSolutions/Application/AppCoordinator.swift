371f742acdebdebda24ba21c12c52a166dd880e4
kolos <kolosqko@gmail.com>
2020-10-01 13:23:36 +0300

    AppCoordinator created

---
+++ TestAppForDefineSolutions/Application/AppCoordinator.swift
@@ -0,0 +1,25 @@
+//
+//  AppCoordinator.swift
+//  TestAppForDefineSolutions
+//
+//  Created by Kolos on 10/1/20.
+//  Copyright © 2020 Kolos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppCoordinator: Coordinator {
+    
+    private let window: UIWindow
+    private let rootViewController: UIViewController
+    
+    init(window: UIWindow) {
+        self.window = window
+        self.rootViewController = UIViewController()
+    }
+    
+    func start() {
+        
+    }
+}
--- TestAppForDefineSolutions/Application/AppDelegate.swift
+++ TestAppForDefineSolutions/Application/AppDelegate.swift
@@ -12,10 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    private var appCoordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        guard let window = self.window else {
+            return true
+        }
+        let appCoordinator = AppCoordinator(window: window)
+        appCoordinator.start()
+        self.appCoordinator = appCoordinator
+        
         return true
     }
 
