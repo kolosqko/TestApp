14f4deafb3c4f1ba280bdab66d8eb65b90b7b5c2
kolos <kolosqko@gmail.com>
2020-10-01 14:10:11 +0300

    Created and pushed into tabbar dashboard viewcontroller

---
+++ TestAppForDefineSolutions/Screens/DashboardViewController/DashboardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DashboardViewController.swift
+//  TestAppForDefineSolutions
+//
+//  Created by Kolos on 10/1/20.
+//  Copyright © 2020 Kolos. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController, StoryboardInstantiable {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- TestAppForDefineSolutions/Screens/TabBar/TabBarCoordinator.swift
+++ TestAppForDefineSolutions/Screens/TabBar/TabBarCoordinator.swift
@@ -13,13 +13,25 @@ class TabBarCoordinator: Coordinator {
     
     private let presenter: UIViewController
     private let tabBarController: UITabBarController
+    private let viewControllersInPresenter: [UIViewController]
+    
+    private var dashboardCoordinator: DashboardCoordinator?
+    private let dashboardNavigationController: UINavigationController
     
     init(presenter: UIViewController) {
         self.presenter = presenter
         self.tabBarController = UITabBarController()
+        
+        self.dashboardNavigationController = UINavigationController()
+        self.viewControllersInPresenter = [dashboardNavigationController]
     }
     
     func start() {
         presenter.present(tabBarController, animated: false)
+        tabBarController.viewControllers = viewControllersInPresenter
+        
+        let dashboardCoordinator = DashboardCoordinator(presenter: dashboardNavigationController)
+        dashboardCoordinator.start()
+        self.dashboardCoordinator = dashboardCoordinator
     }
 }
